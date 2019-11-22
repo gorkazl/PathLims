9d55c018d9916308ec0896a05c34680d6046b4f8
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2019-11-22 22:21:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
     keywords = 'graph theory, complex networks, network analysis',
     classifiers = [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
